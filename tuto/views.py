3e1201b0d07dd6487744dc1e5d6e33ad14d4ca2d
Arthur Goudal <arthur.goudal@etu.univ-orleans.fr>
2023-09-22 12:14:55 +0200

    données livres Amazon

--- tuto/views.py
+++ tuto/views.py
@@ -1,10 +1,11 @@
 from .app import app
 from flask import render_template
+from .models import get_sample
 
 @app.route("/")
 def home():
     return render_template(
         "home.html",
         title = "Hello World",
-        names = ["Pierre", "Paul", "Jacques"]
+        books = get_sample()
     )
